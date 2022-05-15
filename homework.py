d44e4372fa798996492f2de13432807b99a7cdaf
Абдиманап Жансерик <abdimanapzanserik@MacBook-Pro-Abdimanap.local>
2022-05-15 12:36:05 +0300

    Initial commit

---
+++ homework.py
@@ -0,0 +1,61 @@
+# Добавить новый элемент типа str в конец списка
+# words = ["awesome", "incredible", "good", "creep"]
+# words
+# words.append("fantastic")
+
+# Добавить новый элемент типа int в конец списка
+# numbers = [1, 2, 3, 4, 5]
+# numbers
+# numbers.append(6)
+# print(numbers)
+
+# Добавить новый элемент типа list в конец списка
+# words_numbers = [1, "awesome", 2, "incredible", 3, "good"]
+# words_numbers
+# words_numbers.append([4, "creep"])
+
+# Добавить новый элемент типа tuple в конец списка
+
+# words_numbers = [1, "awesome", 2, "incredible", 3, "good"]
+# words_numbers
+# to_do_list = { "first thing": "running"}
+# words_numbers.append(to_do_list.values())
+
+# Создать произвольный словарь
+# Добавить новый элемент с ключом типа str и значением типа int
+
+# d1 = dict(Kazakhstan = "Astana", Russia = "Moscow", China =  "Pekin")
+# d1["USA"] = 50
+# print(d1)
+
+# d1 = dict(Kazakhstan = "Astana", Russia = "Moscow", China =  "Pekin")
+# d1[to_do_list = { "first thing": "running"}] = "running"
+# print(d1)
+
+# d1 = dict(Kazakhstan = "Astana", Russia = "Moscow", China =  "Pekin")
+# del d1["China"]
+# print(d1)
+
+# d1 = dict(Kazakhstan = "Astana", Russia = "Moscow", China =  "Pekin")
+# d1["China"]
+
+
+# d1 = dict(Kazakhstan = "Astana", Russia = "Moscow", China =  "Pekin")
+# d1.keys()
+
+
+
+# 3 - задача
+# def person():
+#     name = input ("What is your name?: ")
+#     old = input ("How old are you?: ")
+#     where_from = input ("Where are you live?")
+#     print("This is", name ,
+#           "It is", old,
+#           "He is from", where_from
+#          )
+# person()
+
+
+
+
---
+++ main.py
@@ -0,0 +1,37 @@
+# # # print ("hello world")
+# # # name = input('Введите своё имя: ')
+# # # print(int(True))
+# #
+# # a=5
+# # b=7
+# # c=8
+# # d=6
+# # a > b and c > d
+#
+# t1 = float(input("Введите температуру в градусах: "))
+# t2 = t1 * 1.8 + 32
+# print("Температура в фаренгейтах:", t2)
+
+# tuple = (1, 3, 5, 6)
+# a = {1, 3, 5, 6}
+#
+# list = [1, 3, 5, 6, 1]
+# b = set(list)
+# print(type(b))
+
+# test_dict = {
+#         "test_login": "123456789",
+#         "ivan": "r1232153", "oleg": "aasdw4"
+#                 ""
+#         }
+# print(test_dict.values())
+
+# test_dict = {
+#         "test_login": "123456789",
+#         "ivan": "r1232153", "oleg": "aasdw4"
+#                 ""
+#         }
+# print(list(test_dict.values())[])
+
+
+
